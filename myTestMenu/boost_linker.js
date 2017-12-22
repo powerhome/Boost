4ab60beb61e023d1c91972f91a83da9fbd212e4b
Patrick McClernan <patmccler@gmail.com>
2017-12-21 20:33:18 -0500

    more refactoring and cleanup. better comments.

--- myTestMenu/boost_linker.js
+++ myTestMenu/boost_linker.js
@@ -1,95 +1,89 @@
 console.log("Content Script Loaded");
 
-var domain = /https?:\/\/(?:www.)?\S*.com|file:\/\/\/\S*.html/i.exec(document.URL)[0];
 //targets not to hit with links
 var invalidTargets = [];
-var replaceWithNum = "#placeholder#";
-var patterns;
+var patternLinkerContainer;
 
 (function setup() {
+	invalidTargets.push(document.body);
 
-//dont let body be a target
-invalidTargets.push(document.body);
+	//patternLinkerContainer holds all the patterns, links, thier text, and the placeholder val for numbers
+	(function setupPatternLinkers() {
 
-//sets up the patterns used to match for links
-//stored in patterns var
-(function setupPatterns() {
-	patterns = new Object();
+		patternLinkerContainer = new Object();
+		let placeholder = "#placeholder#";
+		patternLinkerContainer["placeholder"] = placeholder;
 
-	var homePatt = /H#(\d{1,8})/igm;
-	var homePatternLinker = new PatternLinker(homePatt, domain + "/homes/" + replaceWithNum, "Home#: ");
-	addPattern("home pattern", homePatternLinker);
+		let patternLinkers = new Object();
+		
+		let domain = /https?:\/\/(?:www.)?\S*.com|file:\/\/\/\S*.html/i.exec(document.URL)[0];
 
-	var phonePatt = /\(?(\d{3})\)?(?: |\-)*(\d{3})\-?(\d{4})/igm;
-	var phonePatternLinker = new PatternLinker(phonePatt, domain + "/homes?page=1&homes_filter[phone_number_cond]=eq&homes_filter[phone_number]=" + replaceWithNum, "Phone#: ");
-	addPattern("phone pattern", phonePatternLinker);
+		var homePatternLinker = new PatternLinker(/H#(\d{1,8})/igm, domain + "/homes/" + placeholder, "Home#: ");
+		addPattern("home pattern", homePatternLinker);
 
-	var projPatt = /(?:^|\b)(3\d\-?\d{5})\b/igm;
-	var projPatternLinker = new PatternLinker(projPatt, domain + "/projects?&q[project_number_eq]=" + replaceWithNum, "Project#: ");
-	addPattern("project pattern", projPatternLinker);
+		var phonePatternLinker = new PatternLinker(/\(?(\d{3})\)?(?: |\-)*(\d{3})\-?(\d{4})/igm, domain + "/homes?page=1&homes_filter[phone_number_cond]=eq&homes_filter[phone_number]=" + placeholder, "Phone#: ");
+		addPattern("phone pattern", phonePatternLinker);
 
-	var apptPatt = /(?:^|\s|[^ht]#)([0-2|4-9])(\d{4,7})\b/igm;
-	var apptPatternLinker = new PatternLinker(apptPatt, domain + "/homes?homes_filter[lead_id_cond]=eq&homes_filter[lead_id]=" + replaceWithNum, "Appt #: ");
-	addPattern("appointment pattern", apptPatternLinker);
+		var projPatternLinker = new PatternLinker(/(?:^|\b)(3\d\-?\d{5})\b/igm, domain + "/projects?&q[project_number_eq]=" + placeholder, "Project#: ");
+		addPattern("project pattern", projPatternLinker);
 
-	var ticketPatt = /\b(?:t(?:icket)? ?#? ?)(\d+)\b/igm;
-	var ticketPatternLinker = new PatternLinker(ticketPatt, domain + "/support/tickets/" + replaceWithNum, "Ticket #:");
-	addPattern("ticket pattern", ticketPatternLinker);
+		var apptPatternLinker = new PatternLinker(/(?:^|\s|[^ht]#)([0-2|4-9])(\d{4,7})\b/igm, domain + "/homes?homes_filter[lead_id_cond]=eq&homes_filter[lead_id]=" + placeholder, "Appt #: ");
+		addPattern("appointment pattern", apptPatternLinker);
 
-	//adds patternlinkers to patterns obj
-	function addPattern(name, patternLinker)
-	{
-		patterns[name] = patternLinker;
-	}
+		var ticketPatternLinker = new PatternLinker(/\b(?:t(?:icket)? ?#? ?)(\d+)\b/igm, domain + "/support/tickets/" + placeholder, "Ticket #:");
+		addPattern("ticket pattern", ticketPatternLinker);
 
-	/* 
-	holds a regex pattern and the proper way to link to that item if it matches
-	added: linkText to use when making link
-	*/ 
-	function PatternLinker(pattern, link, linkText)
-	{
-		this.pattern = pattern;
-		this.link = link;
-		this.linkText = linkText;
-	}
+		//store patternLinkers in PLC
+		patternLinkerContainer["patternLinkers"] = patternLinkers;
 
+		//adds patternlinkers to patternLinkers obj
+		function addPattern(name, patternLinker)
+		{
+			patternLinkers[name] = patternLinker;
+		}
+
+		/* 
+		holds a regex pattern and the proper way to link to that item if it matches
+		added: linkText to use when making link
+		*/ 
+		function PatternLinker(pattern, link, linkText)
+		{
+			this.pattern = pattern;
+			this.link = link;
+			this.linkText = linkText;
+		}
+
+	})();
+
+	//sets up listener to get command press from BG Script
+	browser.runtime.onMessage.addListener(request => {
+		console.log("from bg: " + request.greeting);
+		var response = "response: ";
+
+		switch(request.greeting) {
+			case "action clicked":	
+				response += "action click recieved";
+				console.log("action click conf");
+				break;
+			case "command pressed":
+				response += "command pressed recieved";
+				//addTextAtMouse();
+				console.log("command press conf");
+				linkifyAtMouseover();
+				break;
+			default:
+				response += "unknown message";
+				console.log("unknown message recieved");
+				break;
+		}
+		return Promise.resolve({answer: response});
+	});
+	console.log("Setup complete");
 })();
-
-//sets up listener to get command press from BG Script
-browser.runtime.onMessage.addListener(request => {
-	console.log("from bg: " + request.greeting);
-	
-	var response = "response: ";
-
-	if(request.greeting == "action clicked")	
-	{
-		response += "action click recieved";
-		console.log("action click conf");
-	}
-	if(request.greeting == "command pressed")
-	{
-		response += "command pressed recieved";
-		//addTextAtMouse();
-		console.log("command press conf");
-		linkifyAtMouseover();
-	}
-
-
-	return Promise.resolve({answer: response});
-});
-
-
-})();
-
-
-function onError(error) {
-  console.log(`Error: ${error}`);
-}
 
 //checks to see that a node is valid
 //if it is, adds to list so it wont be again
-function validateNode(elem)
-{
+function validateNode(elem) {
 	if(invalidTargets.includes(elem) ){
 		console.log("invalid elem: " + elem);
 		return false;
@@ -107,22 +101,19 @@ function validateNode(elem)
 	Sets all nodes to invalid targets once they are visited once to avoid dups
 */
 function linkifyAtMouseover() {
-
 	console.log("Linkify running");
 
 	let target = getMouseoverElement();
 	let nextArray = [target];
 	let resultDiv = buildResultDiv();
 
-	//loops through nodes -elem get children, text check for matches
+	//loops through nodes -elem add children to list, text check for matches
 	while(nextArray.length > 0)
 	{
-
 		let node = nextArray.pop();
-		if(!validateNode(node)) node = false; //if node is not valid, dont eval the rest
+		if(!validateNode(node)) node = false; //if node is not valid, dont eval it at all
 			switch(node.nodeType) {
-
-				case Node.ELEMENT_NODE: {
+				case Node.ELEMENT_NODE:
 					let children = node.childNodes;
 					//adds the children nodes of current node that need to be checked
 					children.forEach( function(currentChild, currentIndex, children) {
@@ -133,10 +124,10 @@ function linkifyAtMouseover() {
 						}
 					});
 					break;
-				}
-			
-				case Node.TEXT_NODE: {
+				case Node.TEXT_NODE: 
+					console.log("test");
 					let links = linksFromText(node.nodeValue);
+					console.log("test");
 					
 					for(let i = 0; i < links.length; i++) {
 						let thisDiv = document.createElement("DIV");
@@ -150,11 +141,11 @@ function linkifyAtMouseover() {
 						invalidTargets.push(thisDiv.childNodes[1]);
 					}
 					break;
-				}
 			}	//switch
 	}	//while
+
 	if(resultDiv.childNodes[1].firstChild !== null){ //if any links have been added
-		target.parentNode.insertBefore(resultDiv, target.nextSibling);
+		target.parentNode.insertBefore(resultDiv, target.nextSibling); //add result div to dom after target
 	}
 
 	function buildResultDiv() {
@@ -176,8 +167,7 @@ function linkifyAtMouseover() {
 		return resultDiv
 	}
 
-	function addToResult(resultDiv, elem)
-	{
+	function addToResult(resultDiv, elem) {
 		resultDiv.childNodes[1].appendChild(elem);
 	}
 }
@@ -198,32 +188,23 @@ function getMouseoverElement() {
 }
 
 /*
-	Checks all patterns in patterns obj against text and returns links for those matches
+	Checks all patternLinkers in patternLinkers obj against text and returns links for those matches
 */
 function linksFromText(text) {
-	console.log("Getting matches from text and making links");
-	
 	//accumulate all the matches
-	var results = [];
-
-	//patterns holds the patterns to match AND the link to fill with the match
-	for(patt in patterns) {
-		let thisPatt = patterns[patt];
-
+	let results = [];
+	//patternLinkers in PLC holds the patterns to match
+	for(patt in patternLinkers = patternLinkerContainer.patternLinkers) {
+		let thisPatt = patternLinkers[patt];
 		let matches = getMatchesFromText(text, thisPatt.pattern);
-
 		// for every match, replace the placeholder with the actual number
 		for(let i = 0; i < matches.length; i++)
 		{
-			
-			//replace REPLACE WITH NUM in link for patt with num from matches
-			let res = thisPatt.link.replace(replaceWithNum, matches[i]);
-
+			//replace placeholder value in link with num from matches
+			let res = thisPatt.link.replace(patternLinkerContainer.placeholder, matches[i]);
 			res = thisPatt.linkText + linkify(res,  matches[i]);
-
 			results.push(res);
 		}
-
 	}
 	console.log("matches made: " + results.length);
 	return results;
@@ -238,11 +219,11 @@ function linksFromText(text) {
 		let resultArray;
 		let results = [];
 
-		//finds all the  matches in the text
+		//finds all the  matches in the text for this pattern
 		while ((resultArray = pattern.exec(text)) !== null) {
 			  result = "";
 
-			  //index 1,2,3... correspond to capture groups
+			  //index 1,2,3... correspond to capture groups in regex
 			  for(let i = 1; typeof resultArray[i] !== 'undefined'; i++)
 			  {
 				result += resultArray[i];
@@ -256,8 +237,7 @@ function linksFromText(text) {
 	/*
 		Takes an address and text for link and builds the tag accordingly
 	*/
-	function linkify(linkAddress, linkText){
-
+	function linkify(linkAddress, linkText) {
 		result = "<a target=\"_blank\" href =\"" + linkAddress + "\">" + linkText + "</a>";
 
 		return result;
