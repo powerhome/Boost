81b2a1757644d9050f14a23fdf70da0d01db32ab
Patrick McClernan <patmccler@gmail.com>
2017-12-28 19:06:35 -0500

    captured form submission and sucessfully print value input to console

--- boost/sidebar.js
+++ boost/sidebar.js
@@ -2,3 +2,27 @@
 
 
 console.log("test");
+
+window.onload = () =>  {
+
+	let form = document.getElementById('smart_search')
+	form.onsubmit = e => {
+
+		console.log("form submitted");
+		formSubmitted(e.target);
+
+		return false;
+	}
+
+
+
+};
+
+
+
+function formSubmitted(form)
+{
+	console.log("processing " + form.name);
+	console.log(form.childNodes[1].value);
+
+}
