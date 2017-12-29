87e37997a903070afcda8c6c8b54563824e12036
Patrick McClernan <patmccler@gmail.com>
2017-12-28 19:13:13 -0500

    small cleanup.

--- boost/sidebar.js
+++ boost/sidebar.js
@@ -1,25 +1,21 @@
-//document.getElementById("smart_search").style
-
-
-console.log("test");
+console.log("sidebar.js loaded");
 
 window.onload = () =>  {
-
-	let form = document.getElementById('smart_search')
+	let form = document.getElementById('smart_search');
 	form.onsubmit = e => {
 
 		console.log("form submitted");
 		formSubmitted(e.target);
 
+		//prevents the normal form submission
 		return false;
 	}
-
-
-
 };
 
 
-
+/*
+	takes an htmlformelement and parses it
+*/
 function formSubmitted(form)
 {
 	console.log("processing " + form.name);
