c8fe92e4c0ea54935c30d66ade770ef07eb8df89
Patrick McClernan <patmccler@gmail.com>
2017-12-28 20:12:00 -0500

    caught response from bg in sb

--- boost/background.js
+++ boost/background.js
@@ -12,6 +12,58 @@ function onError(error) {
   console.log(`Error: ${error}`);
 }
 
+var patternLinkerContainer;
+
+(function setupPatternLinkers() {
+
+    patternLinkerContainer = new Object();
+    let placeholder = "#placeholder#";
+    patternLinkerContainer["placeholder"] = placeholder;
+
+    let patternLinkers = new Object();
+    
+    let domain = "test";//https?:\/\/(?:www.)?\S{1,30}.com\/|file:\/\/\/\S*.html/i.exec(document.URL)[0];
+    patternLinkerContainer["domain"] = domain;
+
+    var homePatternLinker = new PatternLinker(/H#(\d{1,8})/igm, domain + "homes/" + placeholder, "Home#: ");
+    addPattern("home pattern", homePatternLinker);
+
+    var phonePatternLinker = new PatternLinker(/\(?(\d{3})\)?(?: |\-)*(\d{3})\-?(\d{4})/igm, domain + "homes?page=1&homes_filter[phone_number_cond]=eq&homes_filter[phone_number]=" + placeholder, "Phone#: ");
+    addPattern("phone pattern", phonePatternLinker);
+
+    var projPatternLinker = new PatternLinker(/(?:^|\b)(3\d)\-?(\d{5})\b/igm, domain + "projects?q[project_number_eq]=" + placeholder, "Project#: ");
+    addPattern("project pattern", projPatternLinker);
+
+    var apptPatternLinker = new PatternLinker(/(?:^|\s|[^ht]#)([0-2|4-9]\d{4,7})\b/igm, domain + "homes?homes_filter[lead_id_cond]=eq&homes_filter[lead_id]=" + placeholder, "Appt #: ");
+    addPattern("appointment pattern", apptPatternLinker);
+
+    var ticketPatternLinker = new PatternLinker(/\b(?:t(?:icket)? ?#? ?)(\d+)\b/igm, domain + "support/tickets/" + placeholder, "Ticket #:");
+    addPattern("ticket pattern", ticketPatternLinker);
+
+    //store patternLinkers in PLC
+    patternLinkerContainer["patternLinkers"] = patternLinkers;
+
+    //adds patternlinkers to patternLinkers obj
+    function addPattern(name, patternLinker)
+    {
+      patternLinkers[name] = patternLinker;
+    }
+
+    /* 
+    holds a regex pattern and the proper way to link to that item if it matches
+    added: linkText to use when making link
+    */ 
+    function PatternLinker(pattern, link, linkText)
+    {
+      this.pattern = pattern;
+      this.link = link;
+      this.linkText = linkText;
+    }
+
+  })();
+
+console.log(patternLinkerContainer);
+
 /*
   adds a listener to messages
 */
@@ -24,11 +76,6 @@ browser.runtime.onMessage.addListener(request => {
       case "hello from sidebar":  
         response += "hi from bg";
         console.log("val received: " + request.value);
-        break;
-      case "command pressed":
-        response += "command pressed recieved";
-        //addTextAtMouse();
-        console.log("command press conf");
         break;
       default:
         response += "unknown message";
--- boost/sidebar.js
+++ boost/sidebar.js
@@ -1,5 +1,13 @@
 console.log("sidebar.js loaded");
 
+/*
+Called when there was an error.
+We'll just log the error here.
+*/
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
 window.onload = () =>  {
 	let form = document.getElementById('smart_search');
 	form.onsubmit = e => {
@@ -18,7 +26,11 @@ window.onload = () =>  {
 */
 function formSubmitted(form)
 {
-	browser.runtime.sendMessage({greeting: "hello from sidebar", value: form.childNodes[1].value});
+	browser.runtime.sendMessage({greeting: "hello from sidebar", value: form.childNodes[1].value}
+		).then(response => {
+      console.log(response.answer);
+    }).catch(onError);
+
 	console.log("processing " + form.name);
 	console.log(form.childNodes[1].value);
 
