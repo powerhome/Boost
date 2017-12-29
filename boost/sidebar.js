a09017cef1cbd2049e9c4f2cb11f4696c82ea6e2
Patrick McClernan <patmccler@gmail.com>
2017-12-29 18:57:23 -0500

    finds matches in search, doesnt display them yet.

--- boost/sidebar.js
+++ boost/sidebar.js
@@ -17,11 +17,9 @@ window.onload = () =>  {
       patternLinkerContainer = response.patternLinkerContainer;
     }).catch(onError);
 
-
-
-
 	let form = document.getElementById('smart_search');
 	form.onsubmit = e => {
+		e.preventDefault();
 
 		console.log("form submitted");
 		formSubmitted(e.target);
@@ -37,12 +35,90 @@ window.onload = () =>  {
 */
 function formSubmitted(form)
 {
-	browser.runtime.sendMessage({greeting: "hello from sidebar", value: form.childNodes[1].value}
-		).then(response => {
-      console.log(response.response);
-    }).catch(onError);
-
 	console.log("processing " + form.name);
-	console.log(form.childNodes[1].value);
+	let value =  form.childNodes[1].value;
+	console.log(value);
+	let links = linksFromText(value);
 
+	addResultsAfterForm(form, links);
+
+}
+
+/*
+
+*/
+function addResultsAfterForm(form, links) {
+
+	let resultDiv = document.getElementById("smart_search_results");
+
+	for(let i = 0; i < links.length; i++)
+	{
+		console.log(links[i]);
+		resultDiv.appendChild(links[i]);
+		console.log("test");
+	}
+
+	console.log(resultDiv);
+	form.parentNode.insertBefore(resultDiv, form.nextSibling);
+
+
+
+}
+
+/*
+	Checks all patternLinkers in patternLinkers obj against text and returns links for those matches
+*/
+function linksFromText(text) {
+	//accumulate all the matches
+	let results = [];
+	//patternLinkers in PLC holds the patterns to match
+	for(patt in patternLinkers = patternLinkerContainer.patternLinkers) {
+		let thisPatt = patternLinkers[patt];
+		let matches = getMatchesFromText(text, thisPatt.pattern);
+		// for every match, replace the placeholder with the actual number
+		for(let i = 0; i < matches.length; i++)
+		{
+			//replace placeholder value in link with num from matches
+			let res = thisPatt.link.replace(patternLinkerContainer.placeholder, matches[i]);
+			res = thisPatt.linkText + linkify(res,  matches[i]);
+			results.push(res);
+		}
+	}
+	console.log("matches made: " + results.length);
+	return results;
+
+	/*
+		gets all the matches for pattern from text
+		returns a concatenation of the capture groups for the patter
+		assumes that the capture group collectively concat to the proper number
+	*/
+	function getMatchesFromText(text, pattern) {
+
+		let resultArray;
+		let results = [];
+
+		//finds all the  matches in the text for this pattern
+		while ((resultArray = pattern.exec(text)) !== null) {
+			  result = "";
+
+			  //index 1,2,3... correspond to capture groups in regex
+			  for(let i = 1; typeof resultArray[i] !== 'undefined'; i++)
+			  {
+			  	if(i == 2) { result += "-"; }
+				result += resultArray[i];
+
+			  }
+			  results.push(result);
+		}
+		return results;
+	}
+
+	/*
+		Takes an address and text for link and builds the tag accordingly
+	*/
+	function linkify(linkAddress, textToLink) {
+		result = "<a target=\"_blank\" href =\"" + linkAddress + "\">" + textToLink + "</a>";
+
+		return result;
+	}
 }
